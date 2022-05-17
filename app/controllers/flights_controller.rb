1cff686f78df6a9bbd608c1d8c27f3a90a341066
Lucho-lja <ljallauca.a@gmail.com>
2022-05-17 11:52:35 -0500

    Add search box

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,20 @@
 class FlightsController < ApplicationController
   def index
+    @departure_airport = params[:departure_airport_id]
+    @arrival_airport = params[:arrival_airport_id]
+    
+    @passenger_count = params[:passenger_count]
+    unless @departure_airport.blank? and @arrival_airport.blank?
+      @start_datetime = Date.parse(params[:start_datetime])
+      @flights = Flight.where(
+        departure_airport_id: @departure_airport,
+        arrival_airport_id: @arrival_airport,
+        start_datetime: (@start_datetime..@start_datetime+1.day)
+      ).order(:start_datetime)
+    else
+      @flights = nil
+    end
+    @airports = Airport.all.order("city").map{ |a| [ a.city, a.id ] }
   end
 
   def new
